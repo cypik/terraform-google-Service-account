43c0a8bd88a1bffe74063d8c4b7d6f5b76ccd744
cypiksuresh <suresh.y@cypik.com>
2024-10-17 16:27:23 +0530

    update resource and resdme.md

--- examples/multiple_service_accounts/example.tf
+++ examples/multiple_service_accounts/example.tf
@@ -9,23 +9,20 @@ provider "google" {
 #####==============================================================================
 module "service-account" {
   source = "./../../"
-
-  name = ["svc-account-first", "svc-account-second"]
-
-  display_name = {
-    "svc-account-first"  = "Service Account One"
-    "svc-account-second" = "Service Account Two"
-  }
-
-  description = {
-    "svc-account-first"  = "Description for Account 1"
-    "svc-account-second" = "Description for Account 2"
-  }
-
-  roles = {
-    "svc-account-first"  = "roles/editor"
-    "svc-account-second" = "roles/viewer"
-  }
-
-  generate_keys = true # Change to false to skip key generation
+  service_account = [
+    {
+      name          = "svc-account-first"
+      display_name  = "First Service Account"
+      description   = "This is the first service account"
+      roles         = ["roles/editor", "roles/viewer", "roles/owner"] # Multiple roles
+      generate_keys = true
+    },
+    {
+      name          = "svc-account-second"
+      display_name  = "Second Service Account"
+      description   = "This is the second service account"
+      roles         = ["roles/editor"] # Single role
+      generate_keys = true
+    }
+  ]
 }
